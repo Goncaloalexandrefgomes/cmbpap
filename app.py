cea02c1be13313df81c60c881a786eeb8e2844f5
goncaloalexandrefgomes <goncalo.gomes1412003@gmail.com>
2021-05-15 22:21:24 +0100

    Layout Feito 2

--- app.py
+++ app.py
@@ -89,6 +89,49 @@ def profile():
 def contactdetail():
     return render_template('app-contact-detail.html')
 
+@app.route('/piano')
+def piano():
+    return render_template('piano.html')
+
+@app.route('/guitarraacustica')
+def guitarraacustica():
+    return render_template('guitarraacustica.html')
+
+
+@app.route('/bateria')
+def bateria():
+    return render_template('bateria.html')
+
+
+@app.route('/canto')
+def canto():
+    return render_template('canto.html')
+
+
+@app.route('/guitarra')
+def guitarra():
+    return render_template('guitarra.html')
+
+
+@app.route('/guitarraeletrica')
+def guitarraeletrica():
+    return render_template('guitarraeletrica.html')
+
+
+@app.route('/cavaquinho')
+def cavaquinho():
+    return render_template('cavaquinho.html')
+
+
+@app.route('/saxofone')
+def saxofone():
+    return render_template('saxofone.html')
+
+
+@app.route('/violino')
+def violino():
+    return render_template('violino.html')
+
 @app.route('/pages-login-2', methods=['GET', 'POST'])
 def pageslogin2():
     erro = None
