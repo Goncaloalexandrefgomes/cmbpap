df1ce1cc7ce0e9503ef81812602fd781ed45ba08
goncaloalexandrefgomes <goncalo.gomes1412003@gmail.com>
2021-05-22 18:17:29 +0100

    classes 2

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 class Database:
-    Host = 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com'
-    Database = 'dv578ps6otspp'
-    User = 'yfcpyqcnbenlnf'
-    Password = 'd66d42f6e7de77088fcd63803946f3695b40dba5ea3835a8b0438a8fd965e4c6'
+    def __init__(self):
+        self.Host = 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com'
+        self.Database = 'dv578ps6otspp'
+        self.User = 'yfcpyqcnbenlnf'
+        self.Password = 'd66d42f6e7de77088fcd63803946f3695b40dba5ea3835a8b0438a8fd965e4c6'
